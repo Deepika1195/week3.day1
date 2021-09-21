a543fcc250dcdda75ff1f6d03d2e1ecf8f1a8ccc
Deepika1195 <90104367+Deepika1195@users.noreply.github.com>
2021-09-21 20:25:03 +0530

    Add files via upload

---
+++ Desktop.java
@@ -0,0 +1,19 @@
+package org.system;
+
+public class Desktop extends Computer {
+	
+	public void desktopSize() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Desktop size");
+
+	}
+	
+public static void main(String[] args) {
+	
+	Desktop obj = new Desktop();
+	obj.desktopSize();
+    obj.computerModel();
+
+}
+}
